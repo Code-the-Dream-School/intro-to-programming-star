bda189ef494865d5725b13c4cf1fce04df678e09
Amirah5 <amirah.munad@gmail.com>
2021-10-27 19:23:58 -0700

    fix the bug in homework 4.2 and resubmitting it

--- js/index.js
+++ js/index.js
@@ -1,32 +1,37 @@
-const today = new Date('October 26, 2021 08:30:00');
+const today = new Date("October 26, 2021 08:30:00");
 
 const thisYear = today.getFullYear();
-const footer = document.querySelector('footer');
-const myName = document.getElementById('myName').innerText;
-
+const footer = document.querySelector("footer");
+const myName = document.getElementById("myName").innerText;
 
 let copyright = document.createElement("p");
 
-
-copyright.innerHTML = myName +' '+ thisYear;
+copyright.innerHTML = myName + " " + thisYear;
 footer.appendChild(copyright);
 
-skills = [
-    "HTML", "CSS", "JavaScript",
-    "Git/versioncontrol",
-    "Responsive-Web-Design",
-    "DOM-Manipulation",
-    "Testing-Debugging",
-    "Problem-Solving",
+const skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "Git/versioncontrol",
+  "Responsive-Web-Design",
+  "DOM-Manipulation",
+  "Testing-Debugging",
+  "Problem-Solving",
 ];
 
-const skillsSection = document.getElementsByTagName('skills').innerText;
-const skillsList = document.querySelector('ul');
+const skillsSection = document.getElementById("skills");
+console.log(skillsSection);
+//const skillsList = document.querySelector("ul");//
+const unorderedList = document.createElement('ul')
 
-for(let i=0; i<=skills.length; i++){
-    let skill = document.createElement("li");
-    skill[i].innerText;
-    skill.appendChild(skillsList);
-    
 
+for (let i = 0; i < skills.length; i++) {
+  const skill = document.createElement("li");
+  skill.innerText = skills[i];
+  unorderedList.appendChild(skill);
 }
+
+skillsSection.appendChild(unorderedList)
+
+
