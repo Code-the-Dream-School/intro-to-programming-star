d15d4d5f81a59777b4886ffde908b845018173db
Amirah5 <amirah.munad@gmail.com>
2021-12-14 20:16:19 -0800

    making changes and updating homework lesson-6-1

--- js/index.js
+++ js/index.js
@@ -88,6 +88,18 @@ githubRequest.addEventListener("load", () => {
   if (githubRequest.readyState === 4) {
     repositories = JSON.parse(githubRequest.responseText);
     console.log(repositories);
+
+    let projectSection = document.getElementById("projects");
+
+    let projectList = projectSection.querySelector("ul");
+
+    for (let i = 0; i < repositories.length; i++) {
+      //console.log(i);
+      let project = document.createElement("li");
+      project.innerText = repositories[i].name;
+      //console.log(repositories[i].name);
+      projectList.appendChild(project);
+    }
   }
 });
 
